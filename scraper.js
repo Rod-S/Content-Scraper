4e716df17449217dd4e9eb1e22916269959e6a52
Rod-S <33836692+Rod-S@users.noreply.github.com>
2018-07-17 13:55:11 -0500

    -remove outdated js files, -add csv creation module and partial csv functionality

--- scraper.js
+++ scraper.js
@@ -1,47 +1,81 @@
-var Crawler = require("crawler");
-var cheerio = require('cheerio');
-var fs = require('fs');
+const cheerio = require("cheerio");
+const Json2csvParser = require('json2csv').Parser;
+const fs = require('fs');
 
-var c = new Crawler({
-    maxConnections : 10,
-    // This will be called for each crawled page
-    callback : function (error, res, done) {
-        if(error){
-            console.log(error);
-        }else{
-            var $ = res.$;
-            // $ is Cheerio by default
-            //a lean implementation of core jQuery designed specifically for the server
-            console.log($("title").text());
+const site = "http://shirts4mike.com/";
+const url = "http://shirts4mike.com/shirts.php";
+
+
+const getContent = function(url) {
+  // return new pending promise
+  return new Promise((resolve, reject) => {
+    // select http or https module, depending on reqested url
+    const lib = url.startsWith('https') ? require('https') : require('http');
+    const request = lib.get(url, (response) => {
+      // handle http errors
+      if (response.statusCode < 200 || response.statusCode > 299) {
+         reject(new Error('Failed to load page, status code: ' + response.statusCode));
+       }
+      // temporary data holder
+      const body = [];
+      // on every content chunk, push it to the data array
+      response.on('data', (chunk) => body.push(chunk));
+      // we are done, resolve promise with those joined chunks
+      response.on('end', () => resolve(body.join('')));
+    });
+    // handle connection errors of the request
+    request.on('error', (err) => reject(err))
+    })
+};
+
+if (!fs.existsSync('./data')) {
+  fs.mkDir
+}
+
+//Begin scraping http://shirts4mike.com/shirts.php
+getContent(url)
+  .then(function(html) {
+
+
+
+    var shirts = [];
+    var $ = cheerio.load(html);
+    $('.products li a').each(function(i, elem){
+      shirts.push(site + elem.attribs.href);
+    });
+    for (let i=0; i < shirts.length; i++) {
+      getContent(shirts[i])
+        .then(function(html) {
+          var $ = cheerio.load(html);
+          var titleShirt = $('.shirt-picture span img').attr('alt');
+          var priceShirt = $('.price').text();
+          var imageUrlShirt = $('.shirt-picture span img').attr('src');
+          var urlShirt = shirts[i];
+
+          const fields = ['Title', 'Price', 'ImageURL', 'URL', 'Time'];
+          let shirtValues = [
+            {
+              "Title": titleShirt,
+              "Price": priceShirt,
+              "ImageURL": imageUrlShirt,
+              "URL": urlShirt,
+              "Time": "Time"
+            }
+          ];
+
+          const json2csvParser = new Json2csvParser({ fields });
+          let csv = json2csvParser.parse(shirtValues);
+          fs.createWriteStream('csv.csv', csv, function(err) {
+            if (err) throw err;
+            console.log('file saved');
+          });
+
+
+          //console.log(html);
+          //console.log('titleShirt: ' + titleShirt);
+          //console.log('priceShirt: ' +priceShirt);
+          //console.log('imageUrlShirt: '+imageUrlShirt);
+          //console.log('urlShirt: '+urlShirt);
         }
-        done();
-    }
-});
-
-// Queue URLs with custom callbacks & parameters
-c.queue([{
-    uri: 'http://shirts4mike.com/shirts.php?id=101',
-    jQuery: true,
-
-    // The global callback won't be called
-    callback: function (error, res, done) {
-        if(error){
-            console.log(error);
-        }else{
-            console.log('Grabbed', res.body.length, 'bytes');
-            const html = res.body;
-            console.log(html);
-            fs.writeFile('html.html',
-              html, function success(){
-                console.log('file write success');
-              });
-
-        }
-        done();
-    }
-}]);
-
-// Queue some HTML code directly without grabbing (mostly for tests)
-c.queue([{
-    html: '<p>This is a <strong>test</strong></p>'
-}]);
+      )};
+  }).catch((err) => console.error(err));
